47051fc1f7840d6bf3c07185d63e2a306d48babf
Aman Ambastha <aambastha@ucsd.edu>
2026-01-31 05:52:10 -0800

    Date/Time Changes

--- main/cgi-bin/hw2/python/hello-html-python.py
+++ main/cgi-bin/hw2/python/hello-html-python.py
@@ -20,7 +20,7 @@ print("<p>This Page Was Generated With The Python Programming Language</p>")
 today = datetime.now()
 IP = os.environ.get('REMOTE_ADDR')
 
-print("<p> Today Is: {}</p>".format(today.strftime("%Y-%m-%d %H:%M:%S")))
+print("<p> Page Generated At (UTC): {}</p>".format(today.strftime("%Y-%m-%d %H:%M:%S")))
 print("<p> Your IP Address Is: {}</p>".format(IP))
 print("<p>Using The Common Gateway Interface (CGI) Protocol - CSE 135 Team Aman Ambastha</p>")
 print("</body>")
